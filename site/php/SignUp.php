3fa84376f08c2dd201051d5c2fae7e58cd3ddcad
Olga Kiseleva <kiseleva-1996@yandex.ru>
2016-11-25 01:44:50 +0300

    create a form for registration
    This reverts commit ea32ca9fbcf60324cb4d60bdd83d93a525da1f2e.

--- site/php/SignUp.php
+++ site/php/SignUp.php
@@ -3,14 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <title>LogIn</title>
-    <base href="http://localhost/Reminder/">
+    <base href="http://localhost/Reminder/"> <!-- корень сайта, определенный внутри этого файла -->
     <link rel="stylesheet" type="text/css" href="css/mainStyle.css">
 </head>
 <body>
+
+
 <h2>Ура! пора регистрироваться</h2>
-echo "занесем все в бд, но пока здесь только надпись";
-$a=2+3;
-echo "2+2= $a";
+<form method="post" action="SignUp.php" id="LOGIN">
+    <fieldset> <!-- заставим форму выглядеть как блок-->
+        <strong>Логин: </strong>
+        <input type="text" name="username" size="15" required value=""> <br>
+        <strong>E-mail: </strong>
+        <input type="email" name="mail" required/> <br>
+        <strong>Пароль:</strong>
+        <input type="password" name="password"><br>
+        <strong>Еще раз пароль:</strong>
+        <input type="password" name="password_repeat"><br>
+    </fieldset>
+    <fieldset>
+        Имя:
+        <input type="text" name="first_name"><br>
+        Фамилия:
+        <input type="text" name="last_name"/><br>
+        Пол:
+        <input type="radio" name="gender" value="female" checked> Male
+        <input type="radio" name="gender" value="male"> Female<br>
+    </fieldset>
+    <button type="submit" name="do_signup">Зарегестрироваться</button>
 </form>
 </body>
 </html>
