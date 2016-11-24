239b8b96330ca8a71dfc77ad3d66aef978a6683e
Olga Kiseleva <kiseleva-1996@yandex.ru>
2016-11-25 00:19:15 +0300

    the skeleton

---
+++ site/php/LogIn.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<base href="http://localhost/Reminder/"> <!-- корень сайта -->
+    <link rel="stylesheet" type="text/css" href="css/mainStyle.css">
+</head>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Asus
+ * Date: 18.11.2016
+ * Time: 15:11
+ */
+
+echo "ура, скоро здесь будет авторизация пользователя";
+?>
+</html>
+
---
+++ site/php/SignUp.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>LogIn</title>
+    <base href="http://localhost/Reminder/">
+    <link rel="stylesheet" type="text/css" href="css/mainStyle.css">
+</head>
+<body>
+<h2>Ура! пора регистрироваться</h2>
+echo "занесем все в бд, но пока здесь только надпись";
+$a=2+3;
+echo "2+2= $a";
+</form>
+</body>
+</html>
