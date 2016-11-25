d47d7f7689451e594bdcc9279f6cdb0f27e942e0
Olga Kiseleva <kiseleva-1996@yandex.ru>
2016-11-26 00:29:06 +0300

    added input validation

--- site/php/SignUp.php
+++ site/php/SignUp.php
@@ -8,18 +8,54 @@
 </head>
 <body>
 
+<?php
+$link = mysqli_connect('localhost', 'root', 'root')or die('Ошибка соединения: ' . mysqli_error());
 
+echo 'Успешно соединились';
+$data = $_POST;
+if (isset($data['do_signup'])) # если клавиша "зарегестрировать была нажата, то проведем процесс регистрации
+{
+    $errors = array(); # проверим на пользовательские ошибки. Если они есть положим в этот массив
+    if (trim($data['username']) == '') # trim-функция, обрезающая лишние пробелы
+    {
+        $data['username']='';
+        $errors[] = "Введите корректный логин!";
+    }
+    if (trim($data['password']) == '')
+    {
+        $data['password']='';
+        $data['password_repeat']='';
+        $errors[] = "Введите пароль!";
+    }
+    if ($data['password'] != $data['password_repeat']) {
+        $errors[] = "Пароли не совпадают!";
+    }
+    if (empty($errors)) {
+        //процесс записи данных в бд, но его пока нет
+
+    } else {
+        echo '<h1>' . array_shift($errors) . '</h1>'; # берем первый элемент массива, показываем и изымаем
+    }
+
+
+}
+
+?>
 <h2>Ура! пора регистрироваться</h2>
-<form method="post" action="SignUp.php" id="LOGIN">
+<form method="post" action="site/php/SignUp.php" id="LOGIN">
     <fieldset> <!-- заставим форму выглядеть как блок-->
         <strong>Логин: </strong>
-        <input type="text" name="username" size="15" required value=""> <br>
+        <input type="text" name="username" required value="<?php
+        echo @$data['username']; ?>"> <br>
         <strong>E-mail: </strong>
-        <input type="email" name="mail" required/> <br>
+        <input type="email" name="mail" required value="<?php
+        echo @$data['mail']; ?>"/> <br>
         <strong>Пароль:</strong>
-        <input type="password" name="password"><br>
+        <input type="password" name="password" value="<?php
+        echo @$data['password']; ?>""><br>
         <strong>Еще раз пароль:</strong>
-        <input type="password" name="password_repeat"><br>
+        <input type="password" name="password_repeat" value="<?php
+        echo @$data['password_repeat']; ?>""><br>
     </fieldset>
     <fieldset>
         Имя:
@@ -30,7 +66,8 @@
         <input type="radio" name="gender" value="female" checked> Male
         <input type="radio" name="gender" value="male"> Female<br>
     </fieldset>
-    <button type="submit" name="do_signup">Зарегестрироваться</button>
+    <button type="submit" name="do_signup">Зарегистрироваться</button>
+
 </form>
 </body>
 </html>
